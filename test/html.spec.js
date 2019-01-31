e765f58f3ad0a10e033917622a68ab5267e2d00c
thisunravisara <thisunravisara@gmail.com>
2019-01-31 09:07:29 +0530

    html spec udated

--- test/html.spec.js
+++ test/html.spec.js
@@ -60,7 +60,7 @@ var htmlParser = require('../src/html');
             result: '<span style="color: blue; font-family: sans-serif; font-size: 13pt;">hi</span><span style="color: red; font-family: sans-serif; text-decoration: underline; font-size: 10pt;"><del>there</del></span>',
         }
     ].forEach(( data, i ) => {
-        test('parse function should parse html to carota format '+ i, () => {
+        test('html function should parse carota format to html  '+ i, () => {
             expect( htmlParser.html( data.run )).toEqual( data.result );
         });
     })
