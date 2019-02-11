1b48ff92431da6c6bbabf34d9da74d218a6962aa
thisunravisara <thisunravisara@gmail.com>
2019-02-11 16:01:14 +0530

    Added tests

--- test/html.spec.js
+++ test/html.spec.js
@@ -1,27 +1,94 @@
+var defaultStyles = {
+    size: 10,
+    font: 'lt_regular',
+    color: 'black',
+    bold: true,
+    italic: false,
+    underline: false,
+    strikeout: false,
+    align: 'left',
+    script: 'normal',
+};
+
 var htmlParser = require('../src/html');
 
     [
         { 
             html: '<span style="color:red">hi</span>',
-            result: [{ color: 'red', font: 'sans-serif', size: 10, text: 'hi' }],
+            result: [{
+                text: 'hi',
+                size: 10,
+                font: 'lt_regular',
+                color: 'red',
+                bold: true,
+                italic: false,
+                underline: false,
+                strikeout: false,
+                align: 'left',
+                script: 'normal',
+            }],
         },
         { 
             html: '<span style="color:blue; font-size:13pt" >hi</span><span style="color:red">there</span>',
             result: [
-                { color: 'blue', font: 'sans-serif', size: 13, text: 'hi' },
-                { color: 'red', font: 'sans-serif', size: 10, text: 'there' },
+                {
+                    text: 'hi',
+                    size: 13,
+                    font: 'lt_regular',
+                    color: 'blue',
+                    bold: true,
+                    italic: false,
+                    underline: false,
+                    strikeout: false,
+                    align: 'left',
+                    script: 'normal',
+                },
+                {
+                    text: 'there',
+                    size: 10,
+                    font: 'lt_regular',
+                    color: 'red',
+                    bold: true,
+                    italic: false,
+                    underline: false,
+                    strikeout: false,
+                    align: 'left',
+                    script: 'normal',
+                }
             ],
         },
         { 
             html: '<span style="color:blue; font-size:13pt" >hi</span><span style="color:red"><del>there</del></span>',
             result: [
-                { color: 'blue', font: 'sans-serif', size: 13, text: 'hi' },
-                { color: 'red', font: 'sans-serif', strikeout: true, size: 10, text: 'there' },
+                {
+                    text: 'hi',
+                    size: 13,
+                    font: 'lt_regular',
+                    color: 'blue',
+                    bold: true,
+                    italic: false,
+                    underline: false,
+                    strikeout: false,
+                    align: 'left',
+                    script: 'normal',
+                },
+                {
+                    text: 'there',
+                    size: 10,
+                    font: 'lt_regular',
+                    color: 'red',
+                    bold: true,
+                    italic: false,
+                    underline: false,
+                    strikeout: true,
+                    align: 'left',
+                    script: 'normal',
+                }
             ],
         }
     ].forEach(( data, i ) => {
         test('parse function should parse html to carota format '+ i, () => {
-            expect( htmlParser.parse( data.html )).toEqual( data.result );
+            expect( htmlParser.parse( data.html, defaultStyles )).toEqual( data.result );
         });
     });
 
